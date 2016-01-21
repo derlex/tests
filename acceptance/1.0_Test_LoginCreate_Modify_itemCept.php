eb96daae9606bd81d3fda2eec0bf1e5907a34806
A.Svevchenko <angie.or.jackie@gmail.com>
2016-01-21 13:25:30 +0200

    Added new tests

---
+++ acceptance/1.0_Test_LoginCreate_Modify_itemCept.php
@@ -0,0 +1,46 @@
+<?php
+$I = new AcceptanceTester($scenario);
+$I->wantTo('log in as regular user');
+$I->amOnPage('/login');
+$I->fillField('Email','111');
+$I->fillField('Password','111');
+$I->click('Sign in');
+$I->see('A valid email address is required.');
+$I->fillField('Email','admin@unionimpact.com');
+$I->fillField('Password','12wsx');
+$I->click('Sign in');
+$I->see('Incorrect email or password.');
+$I->fillField('Email','admin@unionimpact.com');
+$I->fillField('Password','1qaz2wsx');
+$I->click('Sign in');
+$I->see('Successfully logged in.');
+$I->click('×');
+$I->see('Members');
+$I->see('Payments');
+$I->see('Account');
+$I->click('Account');
+$I->see('Administrator');
+$I->see('Profile');
+$I->see('Logout');
+//Create item test
+$I->click('Payments');
+$I->see('Dashboard');
+$I->see('Members');
+$I->see('Items');
+$I->see('Payment Gateway');
+$I->see('Reports');
+$I->see('Settings');
+$I->see('Billing');
+$I->click('Settings');
+$I->see('Items');
+$I->see('Groups');
+$I->see('Group Packs');
+$I->see('Process Charges');
+$I->see('Global');
+$I->see('Show All');
+$I->see('ID');
+$I->click ('ID');
+$I->see('Description');
+$I->see('Category');
+$I->see('Item Type');
+?>
